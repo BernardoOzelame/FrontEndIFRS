0954797c992d4587add0653c3ce6aaa19fa8956e
Bernardo Ozelame Spoltti <104913275+BernardoOzelame@users.noreply.github.com>
2024-11-18 14:14:31 -0300

    Tela login (#2)
    * (EM ANDAMENTO) tela login
    
    * Tela login quase finalizada
    
    * pequenas alterações tela login
    
    * layout tela login finalizado, mas sem as devidas funcionalidades

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,12 +4,12 @@ import "./Header.css";
 
 const Header = () => {
     return (
-        <header>
-            <div>
-                <img src="./../../public/ifrs_branco.png" alt="" />
+        <header className="header">
+            <div className="divLogoHeader">
+                <img src="./../../public/ifrs_branco_completo.png" alt="Logo IFRS" className="logoHeader" />
             </div>
-            <div>
-                <p>Você não está autenticado.</p>
+            <div className="autenticacao">
+                <p>Você ainda não está se identificou.</p>
             </div>
         </header>
     );
