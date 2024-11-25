d5b40fbf3b6b8535c4c56d5850fa700e6c510615
Sharlyne Rodrigues de Souza <sharlynerodrigues2@gmail.com>
2024-11-25 15:18:59 -0300

    criado componente adicionarUsuario (rota feita)

---
+++ src/components/AdicionarUsuario/index.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Header from "../Header";
+
+const AdicionarUsuario = () => {
+    return (
+        <>
+            <Header/>
+        </>
+    )
+}
+
+export default AdicionarUsuario;
